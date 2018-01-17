9b3ea2e2a3bb47e7ea87d1b7480f556bb8b30e31
JackSilburn <jack.silburn@live.com.au>
2018-01-17 16:37:20 +0930

    adding dlls

---
+++ Source/Database/Tables/dbo.UserTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserTable] (
+    [TransactionID]          INT      NOT NULL,
+	[UserID]			INT NOT NULL,
+	[UserName]		  TEXT NOT NULL,
+    [Date]            DATETIME NOT NULL,
+    [TransactionCost] MONEY    NOT NULL,
+    [TotalItems]      INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([TransactionID] ASC) 
+);
+
